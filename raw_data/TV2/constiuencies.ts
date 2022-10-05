bf82eefff8b32c0ef763e5b7b23565beb4c2b6ef
cfblaeb <laeb@dtu.dk>
2022-10-05 23:56:33 +0200

    Har noget TV2 data. Vil gerne havde noget DR data. Tester visualiseringen.

---
+++ raw_data/TV2/constiuencies.ts
@@ -0,0 +1,462 @@
+export const constituencies = [
+  {
+    value: 9000702,
+    label: 'Aabenraakredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000402,
+    label: 'Aakirkebykredsen',
+    parent: 9000400,
+  },
+  {
+    value: 9001009,
+    label: 'Aalborg Nordkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9001007,
+    label: 'Aalborg Østkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9001008,
+    label: 'Aalborg Vestkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9000803,
+    label: 'Aarhus Nordkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000804,
+    label: 'Aarhus Østkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000801,
+    label: 'Aarhus Sydkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000802,
+    label: 'Aarhus Vestkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000604,
+    label: 'Assenskredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000208,
+    label: 'Ballerupkredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9000106,
+    label: 'Bispebjergkredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000206,
+    label: 'Brøndbykredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9001003,
+    label: 'Brønderslevkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9000107,
+    label: 'Brønshøjkredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000805,
+    label: 'Djurskredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000305,
+    label: 'Egedalkredsen',
+    parent: 9000300,
+  },
+  {
+    value: 9000704,
+    label: 'Esbjerg Bykredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000705,
+    label: 'Esbjerg Omegnskredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000608,
+    label: 'Faaborgkredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000110,
+    label: 'Falkonerkredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000808,
+    label: 'Favrskovkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000505,
+    label: 'Faxekredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000302,
+    label: 'Fredensborgkredsen',
+    parent: 9000300,
+  },
+  {
+    value: 9000710,
+    label: 'Fredericiakredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9001001,
+    label: 'Frederikshavnkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9000304,
+    label: 'Frederikssundkredsen',
+    parent: 9000300,
+  },
+  {
+    value: 9000201,
+    label: 'Gentoftekredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9000203,
+    label: 'Gladsaxekredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9000507,
+    label: 'Grevekredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000502,
+    label: 'Guldborgsundkredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000713,
+    label: 'Haderslevkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000811,
+    label: 'Hedenstedkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000301,
+    label: 'Helsingørkredsen',
+    parent: 9000300,
+  },
+  {
+    value: 9000909,
+    label: 'Herning Nordkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000908,
+    label: 'Herning Sydkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000303,
+    label: 'Hillerødkredsen',
+    parent: 9000300,
+  },
+  {
+    value: 9001005,
+    label: 'Himmerlandkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9001002,
+    label: 'Hjørringkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9000509,
+    label: 'Holbækkredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000910,
+    label: 'Holstebrokredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000810,
+    label: 'Horsenskredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000205,
+    label: 'Hvidovrekredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9000907,
+    label: 'Ikastkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000103,
+    label: 'Indre Bykredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000510,
+    label: 'Kalundborgkredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000506,
+    label: 'Køgekredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000711,
+    label: 'Kolding Nordkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000712,
+    label: 'Kolding Sydkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000501,
+    label: 'Lollandkredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000202,
+    label: 'Lyngbykredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9001006,
+    label: 'Mariagerfjordkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9000605,
+    label: 'Middelfartkredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000504,
+    label: 'Næstvedkredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000105,
+    label: 'Nørrebrokredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000606,
+    label: 'Nyborgkredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000601,
+    label: 'Odense Østkredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000603,
+    label: 'Odense Sydkredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000602,
+    label: 'Odense Vestkredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000101,
+    label: 'Østerbrokredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000806,
+    label: 'Randers Nordkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000807,
+    label: 'Randers Sydkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000911,
+    label: 'Ringkøbingkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000511,
+    label: 'Ringstedkredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000204,
+    label: 'Rødovrekredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9000401,
+    label: 'Rønnekredsen',
+    parent: 9000400,
+  },
+  {
+    value: 9000508,
+    label: 'Roskildekredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000306,
+    label: 'Rudersdalkredsen',
+    parent: 9000300,
+  },
+  {
+    value: 9000905,
+    label: 'Silkeborg Nordkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000906,
+    label: 'Silkeborg Sydkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000809,
+    label: 'Skanderborgkredsen',
+    parent: 9000800,
+  },
+  {
+    value: 9000902,
+    label: 'Skivekredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000512,
+    label: 'Slagelsekredsen',
+    parent: 9000500,
+  },
+  {
+    value: 9000111,
+    label: 'Slotskredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000701,
+    label: 'Sønderborgkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000901,
+    label: 'Struerkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000104,
+    label: 'Sundbyøsterkredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000102,
+    label: 'Sundbyvesterkredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000607,
+    label: 'Svendborgkredsen',
+    parent: 9000600,
+  },
+  {
+    value: 9000207,
+    label: 'Taastrupkredsen',
+    parent: 9000200,
+  },
+  {
+    value: 9000112,
+    label: 'Tårnbykredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9001004,
+    label: 'Thistedkredsen',
+    parent: 9001000,
+  },
+  {
+    value: 9000703,
+    label: 'Tønderkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000108,
+    label: 'Valbykredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000706,
+    label: 'Vardekredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000707,
+    label: 'Vejenkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000708,
+    label: 'Vejle Nordkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000709,
+    label: 'Vejle Sydkredsen',
+    parent: 9000700,
+  },
+  {
+    value: 9000109,
+    label: 'Vesterbrokredsen',
+    parent: 9000100,
+  },
+  {
+    value: 9000904,
+    label: 'Viborg Østkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000903,
+    label: 'Viborg Vestkredsen',
+    parent: 9000900,
+  },
+  {
+    value: 9000503,
+    label: 'Vordingborgkredsen',
+    parent: 9000500,
+  },
+];
